dd2490396a47305e9d7c892a3a8286d905d806a5
devilyouwei <devilyouwei@gmail.com>
2023-08-08 14:33:56 +0800

    update using cuda run

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import sys
 import torch
-import numpy
 import uvicorn
 from transformers import AutoTokenizer, AutoModel
 from fastapi import FastAPI, Form
 from fastapi.middleware.cors import CORSMiddleware
+
+device = f"cuda:{sys.argv[1]}" if torch.cuda.is_available() and len(
+    sys.argv) >= 2 else "cpu"
 
 app = FastAPI()
 app.add_middleware(
@@ -14,44 +17,44 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# import bert model
-tokenizer = AutoTokenizer.from_pretrained("./model")
-model = AutoModel.from_pretrained("./model")
-
 
 @app.post("/embedding")
 async def embedding(text: str = Form(...)):
+    global model, tokenizer, device
     print(text)
     input = tokenizer(text, truncation=True,
                       padding="max_length", max_length=512)['input_ids']
     print(input)
-    embeddings = model(torch.tensor(input)[None, :])[0]
-    output = embeddings.detach().numpy().tolist()
+    embeddings = model(torch.tensor(input).to(device)[None, :])[0]
+    output = embeddings.cpu().detach().numpy().tolist()
     return {"embedding": output[0]}
 
 
 @app.post("/embeddingMax")
 async def embeddingMax(text: str = Form(...)):
+    global model, tokenizer, device
     print(text)
     input = tokenizer(text, truncation=True, max_length=512)['input_ids']
     print(input)
-    embeddings = model(torch.tensor(input)[None, :])[0]
-    output = embeddings.max(1)[0].detach().numpy().tolist()
+    embeddings = model(torch.tensor(input).to(device)[None, :])[0]
+    output = embeddings.max(1)[0].cpu().detach().numpy().tolist()
     return {"embedding": output[0]}
 
 
 @app.post("/embeddingAvg")
 async def embeddingAvg(text: str = Form(...)):
+    global model, tokenizer, device
     print(text)
     input = tokenizer(text, truncation=True, max_length=512)['input_ids']
     print(input)
-    embeddings = model(torch.tensor(input)[None, :])[0]
-    output = embeddings.mean(1).detach().numpy().tolist()
+    embeddings = model(torch.tensor(input).to(device)[None, :])[0]
+    output = embeddings.mean(1).cpu().detach().numpy().tolist()
     return {"embedding": output[0]}
 
 
 @app.post("/tokenize")
 async def tokenize(text: str = Form(...)):
+    global tokenizer
     print(text)
     input = tokenizer(text, truncation=True, padding="max_length",
                       max_length=512)['input_ids']
@@ -63,5 +66,12 @@ async def tokenize(text: str = Form(...)):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000,
-                log_level="info", reload=True)
+
+    # import bert model
+    tokenizer = AutoTokenizer.from_pretrained("./model")
+    model = AutoModel.from_pretrained("./model")
+
+    print('device', device)
+    model = model.to(device)
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
